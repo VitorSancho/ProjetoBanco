aad19e2d70edc8074d60cf2297a0ef586668ca75
VitorSancho <vitor.sancho07@gmail.com>
2021-11-19 08:34:22 -0300

    primeiroPull

---
+++ TestandoGitHub/TestandoGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestandoGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
